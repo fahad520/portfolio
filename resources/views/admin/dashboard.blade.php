9fbb0c57881bce7f60cb6ee692f5b1e9a2d0abde
Fahad Khalid <fahadkhalid@redsignal.biz>
2019-04-29 13:26:35 +0500

    add js files in admin.master.layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,7 @@
 @extends('layout.admin_master')
+@section('js')
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button);
+    </script>
+    @endsection
